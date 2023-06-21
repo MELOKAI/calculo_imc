0d47f0f0ec66e92f6e92b3368c9839a1a6989347
MELOKAI <134931552+MELOKAI@users.noreply.github.com>
2023-06-21 10:24:29 -0300

    Add files via upload

--- src/components/griditem/index.tsx
+++ src/components/griditem/index.tsx
@@ -1,6 +1,20 @@
-export const griditem = () => {
+import {Level, calculateImc} from '../../helpers/imc'
+import styles from './griditem.module.css'
+import downImage from '../../assets/down.png' 
+import upImage from '../../assets/up.png'
+type Props ={
+    item: Level
+}
+
+export const GridItem = ({item}: Props) => {
 
     return(
-        <div></div>
-    )
-}
+        <div className= {styles.main} style= {{backgroundColor:item.color}}>   
+        <div className= {styles.gridIcon}>    
+
+    {item.icon === 'up' && <img src= {upImage} alt='' width="30"/>}
+    {item.icon === 'down' && <img src= {downImage} alt='' width="30"/> }
+</div>
+     
+    </div>
+)}
